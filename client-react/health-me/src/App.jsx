6c5727c105a40f2f3420312ebfb039e858944efe
Julian Razif Figaro <julian.razif@gmail.com>
2022-08-21 10:47:40 +0700

    first commit: add react client

---
+++ client-react/health-me/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <h1>Hello World!</h1>
+    </div>
+  );
+};
+
+export default App;
